1e90522ba17a0fb2a8ee0937b193a8dae0f6fa65
lish <lalalalatias@gmail.com>
2021-11-14 20:07:12 +0900

    a

--- src/Music/getMusicList.tsx
+++ src/Music/getMusicList.tsx
@@ -1,7 +1,7 @@
 export default function getMusicList() {
   return [
     new MusicInfo( "in the bed all day", "aa", "01 - LimitShield - in the bed all day.mp3" ),
-    new MusicInfo( "honobono0 remix", "", "02 - sanctunes - honobono0 remix.mp3" ),
+    new MusicInfo( "honobono0 remix", "", "02 - （不明なアーティスト） - honobono0 remix.mp3" ),
     new MusicInfo( "マリン・ポリューション (Asian Rock Remix)", "運命のドツボ", "03 - 運命のドツボ - マリン・ポリューション (Asian Rock Remix).mp3" ),
     new MusicInfo( "マリン・ポリューション (LimitShield remix)", "LimitShield", "04 - LimitShield - マリン・ポリューション (LimitShield remix).mp3" ),
     new MusicInfo( "マリン・ポリューション (5h4ru Remix)", "5h4ru", "05 - 5h4ru - マリン・ポリューション (5h4ru Remix).mp3" )
